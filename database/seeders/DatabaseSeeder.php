75152fcbb6e69ec8813aff5ecb9087efced90856
Ziadashraf98 <ziadshalaby98@gmail.com>
2022-11-12 02:08:19 +0200

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(PermissionTableSeeder::class);
-        $this->call(CreateAdminUserSeeder::class);
+        $this->call([
+            PermissionTableSeeder::class,
+            CreateAdminUserSeeder::class
+        ]);
     }
 }
